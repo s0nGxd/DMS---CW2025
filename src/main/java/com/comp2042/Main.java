0a07d104a694fec01de8aced1ba898a5bf283e04
s0nGxd <zynexsong@gmail.com>
2025-11-11 17:13:54 +0800

    Initial Window Size Change
    Changing the initial window size to fit the playfield.

--- src/main/java/com/comp2042/Main.java
+++ src/main/java/com/comp2042/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
         GuiController c = fxmlLoader.getController();
 
         primaryStage.setTitle("TetrisJFX");
-        Scene scene = new Scene(root, 300, 510);
+        Scene scene = new Scene(root, 300, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
         c.setStage(primaryStage);
